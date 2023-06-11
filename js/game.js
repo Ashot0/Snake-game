9a1849c4f80c210be341437a56e67da3add2ad8f
Ashot0 <illia.holovan@gmail.com>
2023-06-11 14:16:33 +0300

    first commit

---
+++ js/game.js
@@ -0,0 +1,11 @@
+
+
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext("2d");
+
+
+const ground = new Image();
+ground.src = "img/bg.png"
+
+const food = new Image();
+food.src = "img/food.png"
