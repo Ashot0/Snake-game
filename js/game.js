10e3961e89f20851ba11c7bb021b7e14e0a927dc
Ashot0 <illia.holovan@gmail.com>
2023-06-12 10:49:47 +0300

    clear files

--- js/game.js
+++ js/game.js
@@ -5,7 +5,17 @@ const ctx = canvas.getContext("2d");
 
 
 const ground = new Image();
-ground.src = "img/bg.png"
+ground.src = "img/bg.png";
 
 const food = new Image();
-food.src = "img/food.png"
+food.src = "img/food.png";
+
+let box = 32;
+
+let score = 0;
+
+function drawGame() {
+	ctx.drawImage(ground, 0, 0);
+}
+
+let game = setInterval(drawGame, 100);
